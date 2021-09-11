eec88e5b2f922d8b02064427357663eccdf984d0
jimkon <dkontzedakis@gmail.com>
2021-09-11 14:58:19 +0100

    tabular q learning for continuous actions spaces

--- my_experiments/QLearning.py
+++ my_experiments/QLearning.py
@@ -7,7 +7,7 @@ from my_experiments.misc import MapFloatToInteger
 
 
 class TabularQLearningAgent(Agent):
-    def __init__(self, points_per_dim, a=0.1, gamma=0.9, epsilon=0.3):
+    def __init__(self, points_per_dim, a=0.9, gamma=0.9, epsilon=1):
         assert points_per_dim >= 2
         self.points_per_dim = points_per_dim
         self.q_table = None
@@ -29,16 +29,16 @@ class TabularQLearningAgent(Agent):
         self.is_action_space_discrete = self.wrapped_env.is_action_space_discrete()
 
         assert self.wrapped_env.action_dims() == 1, 'QLearningAgent is not working for environments with action space with more than one dimensions.'
-
+        self.action_mapper = None
         if self.is_action_space_discrete:
             n_action_points = self.wrapped_env.action_high[0]-self.wrapped_env.action_low[0]+1
             self.actions_mapping_f = lambda x: x
         else:
             n_action_points = self.points_per_dim
-            mapper = MapFloatToInteger(low=self.wrapped_env.action_low[0],
+            self.action_mapper = MapFloatToInteger(low=self.wrapped_env.action_low[0],
                                        high=self.wrapped_env.action_high[0],
                                        n=n_action_points)
-            self.actions_mapping_f = lambda x: mapper.map(x)
+            self.actions_mapping_f = lambda x: self.action_mapper.map(x)
 
         print(n_action_points)
         self.q_table = np.ones(shape=[self.points_per_dim]*self.state_dims+[n_action_points])*0.8
@@ -76,28 +76,37 @@ class TabularQLearningAgent(Agent):
 
     def act(self, state):
         if np.random.uniform() < self.epsilon:
-            action = self.wrapped_env.random_action()
+            # action = self.wrapped_env.random_action()
+            action_i = 0 if state[1] < 0 else 2
+        else:
+            action_i = np.argmax(self.__q_of_state(state))
+
+        if not self.is_action_space_discrete:
+            action = [self.action_mapper.map_out(action_i)]
 
         else:
-            action = np.argmax(self.__q_of_state(state))
-            if not self.is_action_space_discrete:
-                action = [action]
+            action = action_i
 
         return action
 
     def observe(self, state, action, reward, next_state, done):
+        if not self.is_action_space_discrete:
+            action_i = int(self.action_mapper.map_in(action[0]))
+        else:
+            action_i = action
+
         if done:
-            new_Q_s_a = reward
+            new_Q_s_a = self.discount_rate * reward
         else:
             Q_s = self.__q_of_state(state)
-            current_Q_s_a = Q_s[action]
+            current_Q_s_a = Q_s[action_i]
             Q_sn = self.__q_of_state(next_state)
             max_Q_sn = np.max(Q_sn)
 
-            new_Q_s_a = current_Q_s_a + self.learning_rate * (reward*self.discount_rate*max_Q_sn-current_Q_s_a)
+            new_Q_s_a = current_Q_s_a + self.learning_rate * (reward+self.discount_rate*max_Q_sn-current_Q_s_a)
 
         index = self.__state_index(state)
-        self.q_table[index][action] = new_Q_s_a
+        self.q_table[index][action_i] = new_Q_s_a
 
         return
 
@@ -108,8 +117,8 @@ class TabularQLearningAgent(Agent):
 if __name__=="__main__":
     import gym
     from rl.core import engine
-    # env = gym.make('MountainCarContinuous-v0')
-    env = gym.make('MountainCar-v0')
-    agent = TabularQLearningAgent(5)
-    engine.run_episodes(env, agent, 10000, verbosity='episode')
+    env = gym.make('MountainCarContinuous-v0')
+    # env = gym.make('MountainCar-v0')
+    agent = TabularQLearningAgent(6)
+    engine.run_episodes(env, agent, 1000, verbosity='episode', render=False)
     print(np.moveaxis(agent.q_table, -1, 0))
