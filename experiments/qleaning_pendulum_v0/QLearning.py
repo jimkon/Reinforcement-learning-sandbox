26e1831442eb1d626649313a566b33d86fa3f5fa
jimkon <dkontzedakis@gmail.com>
2021-10-07 20:52:05 +0100

    reorganise expreriment

--- experiments/qleaning_pendulum_v0/QLearning.py
+++ experiments/qleaning_pendulum_v0/QLearning.py
@@ -3,7 +3,7 @@ import numpy as np
 from rl.core.agents import Agent
 from rl.core.envs import wrap_env
 
-from experiments.misc import MapFloatToInteger
+from experiments.qleaning_pendulum_v0.misc import MapFloatToInteger
 
 
 class TabularQLearningAgent(Agent):
@@ -113,38 +113,3 @@ class TabularQLearningAgent(Agent):
 
     def name(self):
         return 'tabular_Q_learning'
-
-
-if __name__=="__main__":
-    import gym
-
-    class RewardWrapper(gym.RewardWrapper):
-        def __init__(self, env):
-            super().__init__(env)
-
-        def step(self, action):
-            next_state, reward, done, _ = super().step(action)
-            x, v = next_state
-            if done and x >= .5:
-                reward = 100
-            else:
-                reward = abs(x+0.5)
-            return next_state, reward, done, _
-
-        def reward(self, rew):
-            return 0
-
-    from rl.core import engine
-    # env = gym.make('MountainCarContinuous-v0')
-    # env = gym.make('MountainCar-v0')
-    # env = RewardWrapper(gym.make('MountainCar-v0'))
-    env = gym.make('Pendulum-v0')
-    agent = TabularQLearningAgent(20, epsilon=.0)
-    engine.run_episodes(env,
-                        agent,
-                        2000,
-                        store_results='dataframe',
-                        experiment_name='pend',
-                        verbosity='episode',
-                        render=False)
-    print(np.moveaxis(agent.q_table, -1, 0))
