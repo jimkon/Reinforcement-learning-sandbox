cd731e68e8ac6c2099cd21d0899ebd1962e78599
jimkon <dkontzedakis@gmail.com>
2022-02-06 17:10:18 +0000

    markdown to html

--- rl/src/core/utilities/file_utils.py
+++ rl/src/core/utilities/file_utils.py
@@ -1,6 +1,8 @@
 from os import makedirs
-from os.path import exists, split
+from os.path import exists, split, splitext, join
 import uuid
+
+from markdown import markdown
 
 from rl.src.core.configs.storage_configs import UNIQUE_STRING_LENGHT
 
@@ -15,3 +17,25 @@ def create_path(path):
 
 def unique_string():
     return uuid.uuid4().hex[:UNIQUE_STRING_LENGHT].upper()
+
+
+def markdown_to_html(abspath):
+    with open(abspath, "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+
+    html = markdown(text)
+
+    path, filename = split(abspath)
+
+    filename, ext = splitext(filename)
+
+    html_abspath = join(path, filename+'.html')
+
+    with open(html_abspath, "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+        output_file.write(html)
+
+    return html_abspath
+
+
+if __name__=="__main__":
+    markdown_to_html(r"C:\Users\jim\PycharmProjects\Reinforcement-learning-sandbox\README.md")
